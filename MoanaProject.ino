c69539305a16b0f146e90ee79d4ef8ae626b8ff5
cpdogbuehi <149627479+cpdogbuehi@users.noreply.github.com>
2023-11-08 16:26:08 -0600

    Update MoanaProject.ino
    Servo and buttons set up

--- MoanaProject.ino
+++ MoanaProject.ino
@@ -1,3 +1,45 @@
-// Moana Project
-// Helloooooo
+#include <Servo.h>
 
+Servo leftServo;
+Servo rightServo;
+Servo tefiServo;
+Servo natureServo;
+const int buttonWave = 2;
+const int buttonTefi = 3;
+const int buttonNature = 4;
+const int buttonBoat = 5;
+const int buttonCliff =6;
+
+void setup() {
+  // wave left
+leftServo.attach(9);
+// wave right
+rightServo.attach(10);
+// tefi turns
+tefiServo.attach(11);
+// nature scene turns
+natureServo.attach(12);
+pinMode(buttonWave, INPUT);
+pinMode(buttonTefi, INPUT);
+pinMode(buttonNature, INPUT);
+}
+
+void loop() {
+  if  (digitalRead(buttonWave) == HIGH) {
+      leftServo.write(180);
+      rightServo.write(180);
+    } else {
+      leftServo.write(0);
+      rightServo.write(0);
+    }
+  if  (digitalRead(buttonTefi) == HIGH) {
+      tefiServo.write(180);
+    } else {
+      tefiServo.write(0);
+    }
+  if  (digitalRead(buttonNature) == HIGH) {
+      natureServo.write(180);
+     } else {
+      natureServo.write(0);
+     }
+  }
